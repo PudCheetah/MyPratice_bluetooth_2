a885bc7cdb7a716889a56876aa406ce2ba8fb256
PudCheetah <wer212131@gmail.com>
2024-09-14 02:15:24 +0800

    介面調適完成

--- app/src/main/java/com/example/mypratice_bluetooth_2/DeviceConsoleActivity/RvAdapter_deviceConsole.kt
+++ app/src/main/java/com/example/mypratice_bluetooth_2/DeviceConsoleActivity/RvAdapter_deviceConsole.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isInvisible
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mypratice_bluetooth_2.databinding.RvItemDeviceConsoleBinding
 
@@ -13,6 +14,7 @@ class RvAdapter_deviceConsole(var viewModel: Viewmodel_DeviceConsole): RecyclerV
         val tv_1_rvItem = itemView.tv1RvItem
         val cv_1_rvitem= itemView.cv1Rvitem
         val tv_source = itemView.tvSource
+        val tv_reply = itemView.TVReply
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -31,21 +33,29 @@ class RvAdapter_deviceConsole(var viewModel: Viewmodel_DeviceConsole): RecyclerV
                 //變更顏色
                 tv_1_rvItem.setBackgroundColor(Color.GREEN)
                 // 置右
-                CL_set_2(cv_1_rvitem, (0.95).toFloat())
-                CL_set_2(tv_source, (1).toFloat())
+                CL_set(cv_1_rvitem, (0.95).toFloat())
+                CL_set(tv_source, (1).toFloat())
+                CL_set(tv_reply, (0.95).toFloat())
             }else{
                 //變更顏色
                 tv_1_rvItem.setBackgroundColor(Color.GRAY)
                 // 置左
-                CL_set_2(cv_1_rvitem, (0.05).toFloat())
-                CL_set_2(tv_source, (0).toFloat())
+                CL_set(cv_1_rvitem, (0.05).toFloat())
+                CL_set(tv_source, (0).toFloat())
+                CL_set(tv_reply, (0).toFloat())
             }
             tv_1_rvItem.text = messageInfo?.message
-            tv_source.text = messageInfo?.sourceType
+            tv_source.text = messageInfo?.sourceAddress
+            if (messageInfo?.sourceType != "local"){
+                tv_reply.isInvisible = true
+            }else{
+                tv_reply.text = messageInfo?.reciveStatus.toString()
+            }
+
         }
     }
     //按照percent(0-1)的比例來變更view的水平位置
-    fun CL_set_2(view: View, percent: Float){
+    fun CL_set(view: View, percent: Float){
         (view.layoutParams as ConstraintLayout.LayoutParams).apply {
             horizontalBias = percent
         }
